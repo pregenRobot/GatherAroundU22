b3f293d13ce1cbd0800629fd6a4407b05f766529
Tamim Azmain <tamimazmain2000@gmail.com>
2017-11-06 16:56:29 +0900

    Updated Carrousel View Added map asa a fragment
    **note** still have not finished sq lite errors

--- app/src/main/java/com/example/android/gatheraround/FeedFragmenttab.java
+++ app/src/main/java/com/example/android/gatheraround/FeedFragmenttab.java
@@ -3,9 +3,20 @@ package com.example.android.gatheraround;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.azoft.carousellayoutmanager.CarouselLayoutManager;
+import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
+import com.azoft.carousellayoutmanager.CenterScrollListener;
+import com.example.android.gatheraround.custom_classes.EventDate;
+import com.example.android.gatheraround.custom_classes.Events;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
 
 /**
  * Created by tamimazmain on 2017/11/01.
@@ -14,11 +25,62 @@ import android.view.ViewGroup;
 public class FeedFragmenttab extends Fragment {
     @Nullable
     @Override
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
         View view = inflater.inflate(R.layout.tabfeedfragment,container,false);
+        ArrayList<Events> temp = new ArrayList<>();
+
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+        temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));temp.add(new Events(new EventDate("2000","11","30","-1","-1","-1","-1","-1",
+                "-1","-1"),"Test",1,new LatLng(1.0,23.0),"Hello",
+                "Hello",Events.CATEGORY_CORPORATE,"HEllo",true));
+
+        final CarouselLayoutManager layoutManager = new CarouselLayoutManager(CarouselLayoutManager.VERTICAL, true);
+        layoutManager.setPostLayoutListener(new CarouselZoomPostLayoutListener());
+
+        final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyler_view);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(new FeedAdapter(temp,getContext()));
+        recyclerView.addOnScrollListener(new CenterScrollListener());
 
 
         return view;
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+
     }
 
 }
