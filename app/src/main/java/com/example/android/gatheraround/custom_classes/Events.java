7ad95f93e975707d79f751f54c5415755ecc71ef
Tamim Azmain <tamimazmain2000@gmail.com>
2017-11-11 13:24:13 +0900

    Working firebase recycler view

--- app/src/main/java/com/example/android/gatheraround/custom_classes/Events.java
+++ app/src/main/java/com/example/android/gatheraround/custom_classes/Events.java
@@ -1,12 +1,15 @@
 package com.example.android.gatheraround.custom_classes;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by tamimazmain on 2017/08/14.
  */
 
-public class Events {
+public class Events implements Parcelable{
 
     private EventDate mDate;
     private String mName;
@@ -33,6 +36,18 @@ public class Events {
         mGlobalId = key;
         mDoesExistsOnServer =doesExistsOnServer;
     }
+
+    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+        @Override
+        public Events createFromParcel(Parcel in) {
+            return new Events (in);
+        }
+
+        @Override
+        public Events[] newArray(int i) {
+            return new Events[i];
+        }
+    };
 
 
     public EventDate getDate(){
@@ -75,7 +90,48 @@ public class Events {
         return mDoesExistsOnServer;
     }
 
-    public String toString(){
-        return  mDate.makeOneLineText() + " / " + mName + " / " + mParticipants + " / " + mLocation.toString() + " / " + mLocationName + " / " + mEventSummary + " / " + mCategory;
+    public Events(Parcel in){
+        this.mDate = in.readParcelable(EventDate.class.getClassLoader());
+        this.mName = in.readString();
+        this.mParticipants = in.readInt();
+        this.mLocation = in.readParcelable(LatLng.class.getClassLoader());
+        this.mLocationName = in.readString();
+        this.mEventSummary = in.readString();
+        this.mGlobalId = in.readString();
+        this.mDoesExistsOnServer = in.readByte() != 0;
+
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeParcelable(this.mDate,flags);
+        dest.writeString(this.mName);
+        dest.writeInt(this.mParticipants);
+        dest.writeParcelable(this.mLocation,flags);
+        dest.writeString(this.mLocationName);
+        dest.writeString(this.mEventSummary);
+        dest.writeString(this.mGlobalId);
+        dest.writeByte((byte) (this.mDoesExistsOnServer ? 1:0));
+    }
+
+    @Override
+    public String toString() {
+        return "Events{" +
+                "mDate='" + mDate + '\'' +
+                ", mName='" + mName + '\'' +
+                ", mParticipants='" + mParticipants + '\'' +
+                ", mLocation='" + mLocation + '\'' +
+                ", mLocationName='" + mLocationName + '\'' +
+                ", mEventSummary='" + mEventSummary + '\'' +
+                ", mGlobalId='" + mGlobalId + '\'' +
+                ", mDoesExistOnServer='" + mDoesExistsOnServer + '\'' +
+                '}';
+    }
+
+
 }
